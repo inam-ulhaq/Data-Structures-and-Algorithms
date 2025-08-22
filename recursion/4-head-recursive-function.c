e98219a31ee8144bd7c27adf531cc1947b9bd7f9
inam-ulhaq <inam88643@gmail.com>
2025-08-22 20:11:24 +0500

    update: head recursion example

--- recursion/4-head-recursive-function.c
+++ recursion/4-head-recursive-function.c
@@ -1,8 +1,18 @@
-// Title: Head Recurisive Function
+// Title: Head Recursion
 // Author: Inam Ul Haq 
+//
 // Discription:
-// A function is head recursive of the recursive is happens first, and then some work is done after the recursive
-// call returns.
+// A function calling itself and the recrsive call is his first statment
+// and what all the processing it has to do is doing after the recursive
+// call then the function is said to be head recursive.
+// mean there is no operation before the recursive call.
+// If a function perform all it's work in decending phase and did nothing 
+// during acending phase then the function is consider is head recursive 
+// function.
+//
+// looking to head recursive functions and convrting it into loops is not
+// esay.
+
 #include <stdio.h>
 
 void headFun(int x){
